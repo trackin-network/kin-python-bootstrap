d09cb41324016fad79c43135036e75ffadc4d61b
Chancey Hawbaker <chancey.hawbaker@petco.com>
2019-07-19 22:19:48 -0700

    asd

--- bootstrap/main.py
+++
@@ -1,8 +0,0 @@
-"""Main entry point"""
-
-from src.config import Settings
-from src.init import init_app
-
-config = Settings()
-app = init_app(config)
-app.run(access_log=False, host='0.0.0.0', port=config.PORT)
